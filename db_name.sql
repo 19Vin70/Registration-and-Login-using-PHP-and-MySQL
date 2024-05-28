f871d67bb2df3d45519a3f1abb9b63ca0cb13c7a
Marvin Quillo Saik <alqasimquillo@gmail.com>
2024-05-28 18:16:34 +0800

    New commit

--- db_name.sql
+++ db_name.sql
@@ -7,3 +7,12 @@ CREATE TABLE `users` (
   `image` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+
+ALTER TABLE `users`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
